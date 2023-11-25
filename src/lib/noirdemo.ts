55e80ef5ebb0026140bada5f355e5efd1967e022
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2023-11-26 00:08:03 +0200

    feat: generate qr code of the proof

---
+++ src/lib/noirdemo.ts
@@ -0,0 +1,22 @@
+import {
+  BarretenbergBackend,
+  CompiledCircuit,
+  ProofData,
+} from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
+import noir_demo from '../../circuits/target/main.json';
+
+const backend = new BarretenbergBackend(noir_demo as CompiledCircuit);
+export const AgeVerifier = new Noir(noir_demo, backend);
+export const MINIMUM_AGE = 18;
+
+export async function generateProof(age: number): Promise<ProofData> {
+  return AgeVerifier.generateFinalProof({
+    age_of_person: age,
+    minimum_age: MINIMUM_AGE,
+  });
+}
+
+export async function verifyProof(proof: ProofData): Promise<boolean> {
+  return AgeVerifier.verifyProof(proof);
+}
--- src/pages/entrant.tsx
+++ src/pages/entrant.tsx
@@ -1,4 +1,17 @@
+import { generateProof } from '../lib/noirdemo';
+import { createSignal, createResource } from 'solid-js';
+import { QRCodeSVG } from 'solid-qr-code';
+import { unpack, pack } from 'msgpackr';
+
 export function EntrantPage() {
+  const [age, setAge] = createSignal<number | undefined>(undefined);
+  const [proof, obj] = createResource(age, async (ageN) => {
+    const proof = await generateProof(ageN);
+    console.log(proof);
+    console.log(pack(proof.proof));
+    return pack(proof.proof);
+  });
+
   return (
     <div class="bg-white p-10 rounded-lg shadow-lg text-center">
       <h1 class="text-2xl font-bold mb-4">ENTRANT</h1>
@@ -7,16 +20,33 @@ export function EntrantPage() {
           ENTER AGE
         </label>
         <input
-          type="text"
+          type="number"
           id="age"
           name="age"
+          value={age()}
           class="border-2 border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:border-purple-500"
           placeholder="Enter your age"
+          onChange={(e) => {
+            const age = parseInt(e.currentTarget.value);
+            setAge(age);
+          }}
         />
       </div>
-      <button class="bg-purple-600 text-white text-lg rounded-lg px-6 py-2 w-full hover:bg-purple-700 focus:outline-none">
+      <button
+        class="bg-purple-600 text-white text-lg rounded-lg px-6 py-2 w-full hover:bg-purple-700 focus:outline-none"
+        onClick={async () => {
+          obj.refetch();
+        }}
+      >
         GENERATE PROOF
       </button>
+      {proof.state === 'errored' && <p class="text-gray-500">Errored...</p>}
+      {proof.state === 'pending' && <p class="text-gray-500">Generating...</p>}
+      {proof.state === 'refreshing' && (
+        <p class="text-gray-500">Refreshing...</p>
+      )}
+      {proof.state === 'unresolved' && <div />}
+      {proof.state === 'ready' && <QRCodeSVG value={proof} />}
     </div>
   );
 }
