7e0add4fcfa480cbee215a0f6ece6291f2d7ffaf
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2023-11-28 23:21:39 +0200

    typefix?

--- src/lib/noirdemo.ts
+++ src/lib/noirdemo.ts
@@ -7,7 +7,7 @@ import { Noir } from '@noir-lang/noir_js';
 import noir_demo from '../../circuits/target/main.json';
 
 const backend = new BarretenbergBackend(noir_demo as CompiledCircuit);
-export const AgeVerifier = new Noir(noir_demo, backend);
+export const AgeVerifier = new Noir(noir_demo as CompiledCircuit, backend);
 export const MINIMUM_AGE = 18;
 
 export async function generateProof(age: number): Promise<ProofData> {
